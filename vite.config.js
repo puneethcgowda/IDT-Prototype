4ca80940bc674f792dc740220f5aeeb3e1b81cc0
Puneeth C Gowda <puneethc0007@gmail.com>
2026-06-13 23:52:20 +0530

    Fix server.allowedHosts configuration

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: [
-      sb-lk49fzwdj928.vercel.run
-    ]
+  server.allowedHosts: {sb-lk49fzwdj928.vercel.run
   }
 })
