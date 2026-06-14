805a91c9511249ff4402cef5ac06e164d941e390
Puneeth C Gowda <puneethc0007@gmail.com>
2026-06-14 23:59:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server.allowedHosts: {sb-lk49fzwdj928.vercel.run
-  }
-})
+  server: { host: true, port: 5173 },
+});
